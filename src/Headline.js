b5246ab32061e8c601b608847323b006ae05cd56
tracht <marija.trachtenberg@gmail.com>
2020-08-18 15:16:22 +0100

    Removes randomness from button click to show next quote.

--- src/Headline.js
+++ src/Headline.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 import './Headline.css';
 
-
-
 const Headline = () => {
 
   const[quote, setQuote] = useState(
-    'The first step is to establish that something is possible; then probability will occur.'
+    "You can either watch it happen or be a part of it."
   );
 
-  const QUOTES = [
-    'The first step is to establish that something is possible; then probability will occur.',
-    // 'When something is important enough, you do it even if the odds are not in your favor.',
-    "I can either watch it happen or be a part of it.",
-    "It's OK to have your eggs in one basket as long as you control what happens to that basket.",
-    "If you go back a few hundred years, what we take for granted today would seem like magic."
-  ]
+  const QUOTES = ([
+    'You can either watch it happen or be a part of it.',
+    'The first step is establishling that something is possible; then probability will occur.',
+    'When something is important you do it even if the odds are not in your favor.',
+    "It's OK to have your eggs in one basket as long as you control what happens to the basket.",
+    'If you go back a few hundred years, what we take for granted today would seem like magic.',
+  ])
 
-  const shuffleQuote = () => {
-    for ( let i = (QUOTES.length - 1); i > 0; i-- ) {
-      const j = Math.floor(Math.random() * (i - 1))
-      const newQuote = QUOTES[i]
-      QUOTES[i] = QUOTES[j]
-      QUOTES[j] = newQuote
-      console.log(newQuote);
-      return setQuote( newQuote );;
-    }    
-  }
+  const nextQuote = (function () {
+    let counter = 0;
+    return function () {
+      if ( counter > QUOTES.length ) {
+        return ( 
+          counter = 0,
+          setQuote(QUOTES[counter]),
+          console.log("Counter is greater than Quotes.length", counter), 
+          counter
+        )
+      } else {
+          return ( 
+            counter +=1,
+            console.log("Counter is less than Quotes.length", counter),
+            setQuote(QUOTES[counter]),
+            counter
+          )
+       };
+    }
+  })();
   
   return (
     <div> 
@@ -34,10 +42,12 @@ const Headline = () => {
       <p> {quote} </p>
       <button 
         className="headline-button" 
-        onClick={shuffleQuote} > 
-        see another quote
+        onClick={nextQuote} > 
+        next quote
       </button>
     </div>
-  )}
+  )
+
+}
 
 export default Headline;
