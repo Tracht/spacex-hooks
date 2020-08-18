8d0bf28519ac71f0f4b3b777e7dd514f76e9c84a
tracht <marija.trachtenberg@gmail.com>
2020-08-18 15:14:10 +0100

    Adds button counter to show next quote - partially works.

--- src/Headline.js
+++ src/Headline.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Headline.css';
 
-function Headline({title, quote}) {
+
+
+const Headline = () => {
+
+  const[quote, setQuote] = useState(
+    'The first step is to establish that something is possible; then probability will occur.'
+  );
+
+  const QUOTES = [
+    'The first step is to establish that something is possible; then probability will occur.',
+    // 'When something is important enough, you do it even if the odds are not in your favor.',
+    "I can either watch it happen or be a part of it.",
+    "It's OK to have your eggs in one basket as long as you control what happens to that basket.",
+    "If you go back a few hundred years, what we take for granted today would seem like magic."
+  ]
+
+  const shuffleQuote = () => {
+    for ( let i = (QUOTES.length - 1); i > 0; i-- ) {
+      const j = Math.floor(Math.random() * (i - 1))
+      const newQuote = QUOTES[i]
+      QUOTES[i] = QUOTES[j]
+      QUOTES[j] = newQuote
+      console.log(newQuote);
+      return setQuote( newQuote );;
+    }    
+  }
+  
   return (
     <div> 
-      <h1> {title} </h1>
+      <h1> SpaceX </h1>
       <p> {quote} </p>
+      <button 
+        className="headline-button" 
+        onClick={shuffleQuote} > 
+        see another quote
+      </button>
     </div>
   )}
 
