a4a2b35f5db7768c0cb8bdd91a4c5e98ff8458d1
tracht <marija.trachtenberg@gmail.com>
2020-09-25 17:25:29 +0100

    Reorders quotes

--- src/App/Components/Homepage/Quotes.js
+++ src/App/Components/Homepage/Quotes.js
@@ -1,18 +1,17 @@
 const QUOTES = ([
-  'You can either watch it happen or be a part of it.',
   'Where are the aliens?',
-  'I would rather commit seppuku than fail.',
+  'You can either watch it happen or be a part of it.',
   'Aliens built the pyramids obv.',
   'I wish there was a way to get nutrients without sitting down for a meal.',
   'They will see their families a lot when we go bankrupt.',
-  'If something is important do it even if the odds are not in your favor.',
-  `Nuke Mars!`,
-  `Theres a billion-to-one chance we're living in base reality`,
   `I'm not an alien ... but I used to be one.`,
+  `Theres a billion-to-one chance we're living in base reality`,
+  `Nuke Mars!`,
   'If you go back a few hundred years, what we take for granted today would seem like magic.',
-  `At least when there's an evil dictator, that human is going to die. But for an AI, there will be no death — it would live forever.`,
   `Oh btw I'm building a cyborg dragon.`,
-  `You're already a cyborg. Most people don't realize you're already a cyborg.`,
+  `At least when there's an evil dictator, that human is going to die. But for an AI, there will be no death — it would live forever.`,
+  `Most people don't realize you're already a cyborg.`,
+  'If something is important do it even if the odds are not in your favor.',
   ]);
 
 export default QUOTES; 
