be7add8b6b3a3f0cea9a9d5a73de2b7ac067283d
tracht <marija.trachtenberg@gmail.com>
2020-09-25 17:17:08 +0100

    Adds more quotes, including funny ones

--- src/App/Components/Homepage/Quotes.js
+++ src/App/Components/Homepage/Quotes.js
@@ -1,8 +1,16 @@
 const QUOTES = ([
   'You can either watch it happen or be a part of it.',
+  'Where are the aliens?',
+  'I would rather commit seppuku than fail.',
+  'Aliens built the pyramids obv.',
+  'I wish there was a way to get nutrients without sitting down for a meal.',
+  'They will see their families a lot when we go bankrupt.',
   'If something is important do it even if the odds are not in your favor.',
+  `Theres a billion-to-one chance we're living in base reality`,
+  `I'm not an alien ... but I used to be one.`,
   'If you go back a few hundred years, what we take for granted today would seem like magic.',
-  "It's OK to have your eggs in one basket as long as you control what happens to the basket.",
+  `At least when there's an evil dictator, that human is going to die. But for an AI, there will be no death — it would live forever.`,
+  `Oh btw I'm building a cyborg dragon.`,
   ]);
 
 export default QUOTES; 
