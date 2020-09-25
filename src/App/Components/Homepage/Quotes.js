9b68013d3193a732c72cbf5b4064cdc8c7a7344d
tracht <marija.trachtenberg@gmail.com>
2020-09-25 17:19:46 +0100

    Adds more funny Elon quotes

--- src/App/Components/Homepage/Quotes.js
+++ src/App/Components/Homepage/Quotes.js
@@ -6,11 +6,13 @@ const QUOTES = ([
   'I wish there was a way to get nutrients without sitting down for a meal.',
   'They will see their families a lot when we go bankrupt.',
   'If something is important do it even if the odds are not in your favor.',
+  `Nuke Mars!`,
   `Theres a billion-to-one chance we're living in base reality`,
   `I'm not an alien ... but I used to be one.`,
   'If you go back a few hundred years, what we take for granted today would seem like magic.',
   `At least when there's an evil dictator, that human is going to die. But for an AI, there will be no death — it would live forever.`,
   `Oh btw I'm building a cyborg dragon.`,
+  `You're already a cyborg. Most people don't realize you're already a cyborg.`,
   ]);
 
 export default QUOTES; 
