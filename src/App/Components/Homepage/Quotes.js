21220eee679331cb2bdff8f3ed34d3791df0f3a1
tracht <marija.trachtenberg@gmail.com>
2020-09-25 17:34:40 +0100

    updates quotes

--- src/App/Components/Homepage/Quotes.js
+++ src/App/Components/Homepage/Quotes.js
@@ -7,11 +7,8 @@ const QUOTES = ([
   `I'm not an alien ... but I used to be one.`,
   `Theres a billion-to-one chance we're living in base reality`,
   `Nuke Mars!`,
-  'If you go back a few hundred years, what we take for granted today would seem like magic.',
   `Oh btw I'm building a cyborg dragon.`,
-  `At least when there's an evil dictator, that human is going to die. But for an AI, there will be no death — it would live forever.`,
   `Most people don't realize you're already a cyborg.`,
-  'If something is important do it even if the odds are not in your favor.',
   ]);
 
 export default QUOTES; 
