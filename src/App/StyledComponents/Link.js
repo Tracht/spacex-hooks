e8d30aaad8850b8ddef4601f9b45e8db7428b043
tracht <marija.trachtenberg@gmail.com>
2020-09-27 09:44:35 +0100

    Increase pagination font size for mobile & tablet

--- src/App/StyledComponents/Link.js
+++ src/App/StyledComponents/Link.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import device from './device';
 
 export default styled.a`
   margin-bottom: 4em; 
   font-size: 1.1em;
   /* text-decoration: underline; */
   color: ${ props => props.green ? "#00cc99" : "white" };
-  /* Text align does not work for hrefs */
   text-align: center;
+
+  @media ${ device.mobileL | device.tablet} { 
+    font-size: 1.5em;
+  }
+
 `;
