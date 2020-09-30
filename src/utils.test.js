e43bd8e663bf7d6c7b50a93cad1c5f0f6c66d91b
tracht <marija.trachtenberg@gmail.com>
2020-09-30 15:16:17 +0100

    Fixes utils test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,21 +1,4 @@
-const sortAtoZ = (array) => {
-  return array.sort(function (a, b) {
-    if ( a.mission_name ) {
-      a = a.mission_name 
-      b = b.mission_name 
-      return a < b ? -1 : a > b ? 1 : 0;
-    } else if ( a.core_serial ) {
-      a = a.core_serial 
-      b = b.core_serial 
-      return a < b ? -1 : a > b ? 1 : 0;
-    } else if ( a.rocket_name ) {
-      a = a.rocket_name 
-      b = b.core_serial 
-      return a < b ? -1 : a > b ? 1 : 0;
-    } 
-    return null 
-  });
-}
+import sortAtoZ from './utils';
 
 const UnorderedArray = [{mission_name: "Zebra"}, {mission_name: "Monkey"}, {mission_name: "Dog"}, {mission_name: "Apple"}, {mission_name: "Rainforest"}]
 
